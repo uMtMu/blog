646e8aa7362a0379eab5d09c72664c8256716f93
uMtMu <umut1010@gmail.com>
2017-01-09 12:08:36 +0300

    bla bla

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
     $(".outline-3").each(function(){
 	if($.inArray($(this).attr("id").substr(24), ["1", "2"]) != -1){
 	    $(this).hide();
