80b8c250e5018e0e857f252d9850681cc4fa2674
umtmu <umut1010@gmail.com>
2017-03-08 10:20:56 +0300

    footer nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
     $(".outline-3").each(function(){
 	if($.inArray($(this).attr("id").substr(24), ["1", "2"]) != -1){
 	    $(this).hide();
 	}
     });
-});
+});*/
