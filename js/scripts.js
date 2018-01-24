c9bd8c47527959d8faebff767473067a40dcf685
uMtMu <umut1010@gmail.com>
2018-01-24 10:32:33 +0300

    page links added to end of page too

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,53 @@
 $(function(){
-    var tableOfContents = $('#table-of-contents');
-    var entries = $('#text-table-of-contents ul li ul li');
-    var y = (a) => console.log(a);
+var tableOfContents = $('#table-of-contents');
+var entries = $('[id^=outline-container-sec-1-]');
+var entry_links = $('#text-table-of-contents ul li ul li');
+var y = (a) => console.log(a);
 
-    var pagePer = 20;
-    var pageActive = 0;
-    var pageCount = Math.ceil(entries.length / pagePer);
+var pagePer = 20;
+var pageActive = 0;
+var pageCount = Math.ceil(entry_links.length / pagePer);
 
-    entries.parent().prepend('<div id="pageLinks"></div>');
-    var pageLinks = $('#pageLinks');
 
-    pageLinks.append(` <a class="pagelink" target="0" id="pagePrev"><</a> `);
-    pageLinks.append(_.map(_.range(0, pageCount), (p) => ` <a class="pagelink" target="${p}" id="page-${p}">${p+1}</a> `));
-    pageLinks.append(` <a class="pagelink" target="${pageCount-1}" id="pageNext">></a> `);
+page_links_html = `<center><br><a class="pagelink" target="0" id="pagePrev"><</a>`
+page_links_html += _.map(_.range(0, pageCount), (p) => `<a class="pagelink" target="${p}" id="page-${p}">${p+1}</a>`).join('');
+page_links_html += `<a class="pagelink" target="${pageCount-1}" id="pageNext">></a><br><br></center>`
 
-    function pageToggle(pA) {
-	start = pA * pagePer;
-	end = start + pagePer;
-	
-	if (pA === pageCount-1) {    
-	    $('#pagePrev').show();
-	    $('#pageNext').hide();
-	} else {
-	    $('#pagePrev').attr('target', `${parseInt(pA)-1}`);
-	    $('#pageNext').attr('target', `${parseInt(pA)+1}`);
-	    
-	    $('#pagePrev').show();
-	    $('#pageNext').show();
-	}
-	
-	entries.hide();
-	entries.slice(start, end).toggle();
-	pageActive = pA;
-    }
+entry_links.parent().prepend('<div id="pageLinks"></div>');
+$('#pageLinks').append(page_links_html);
+$('#outline-container-sec-1').append(page_links_html);
 
-    $(".pagelink").click(function(e) {
-	// !!!!!!!!!!!!!!
-	pageNumber = $(e.target).attr('target');
-	if (pageNumber == parseInt(pageNumber, 10) && pageNumber < pageCount && pageNumber >= 0) {
-	    pageToggle(pageNumber)
-	}
-    });
+function pageToggle(pA) {
+  start = pA * pagePer;
+  end = start + pagePer;
+  
+if (pA === pageCount-1) {    
+    $('#pagePrev').show();
+    $('#pageNext').hide();
+  } else {
+    $('#pagePrev').attr('target', `${parseInt(pA)-1}`);
+    $('#pageNext').attr('target', `${parseInt(pA)+1}`);
+    
+    $('#pagePrev').show();
+    $('#pageNext').show();
+  }
+  
+  entry_links.hide();
+  entry_links.slice(start, end).toggle();
+  entries.hide();
+  entries.slice(start, end).toggle();
+  pageActive = pA;
+}
 
-    tableOfContents.find('h2:first').text('İçindekiler');
+$(".pagelink").click(function(e) {
+  // !!!!!!!!!!!!!!
+  pageNumber = $(e.target).attr('target');
+  if (pageNumber == parseInt(pageNumber, 10) && pageNumber < pageCount && pageNumber >= 0) {
+    pageToggle(pageNumber)
+  }
+});
 
-    pageToggle(0);
+tableOfContents.find('h2:first').text('İçindekiler');
+
+pageToggle(0);
 });
